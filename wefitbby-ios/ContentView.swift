2d430a0a79d3f13732f96a25bdbb8fd8f0288fef
avintech <rongk4ii@gmail.com>
2020-06-19 13:58:02 +0800

    Initial Commit

---
+++ wefitbby-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wefitbby-ios
+//
+//  Created by avintech on 19/6/20.
+//  Copyright © 2020 avintech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wefitbby-iosTests/wefitbby_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  wefitbby_iosTests.swift
+//  wefitbby-iosTests
+//
+//  Created by avintech on 19/6/20.
+//  Copyright © 2020 avintech. All rights reserved.
+//
+
+import XCTest
+@testable import wefitbby_ios
+
+class wefitbby_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
